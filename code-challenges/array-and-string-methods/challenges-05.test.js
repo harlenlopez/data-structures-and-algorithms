5c77b876baf8f35da6375ce0fcfe5d6630fe2c68
harlen lopez <harlen.d.lopez@gmail.com>
2020-01-18 21:10:05 -0800

    thought i had 3 down but cant get it to work

--- code-challenges/array-and-string-methods/challenges-05.test.js
+++ code-challenges/array-and-string-methods/challenges-05.test.js
@@ -15,7 +15,9 @@ For example, if the input is 'Welcome', the output will be:
 
 const howMuchPencil = (str) => {
   let result = [];
-  // Solution code here...
+  for (let i = 0; i <= str.length; i++) {
+    result.push(str.slice(i));
+  }
   return result;
 };
 
@@ -27,9 +29,7 @@ Write a function name wordsToCharList that, given a string as input, returns a n
 For example, wordsToCharList('gregor') returns ['g','r','e','g','o','r'].
 ------------------------------------------------------------------------------------------------ */
 
-const wordsToCharList = (arr) => {
-  // Solution code here...
-};
+const wordsToCharList = (arr) => arr.split('');
 
 
 /* ------------------------------------------------------------------------------------------------
@@ -75,7 +75,7 @@ const gruffaloCrumble = {
 
 const listFoods = (recipe) => {
   let result = [];
-  // Solution code here...
+  recipe.forEach(string => result.push(string.pop()));
   return result;
 };
 
