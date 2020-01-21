1c48fcd7839c50fc22578fb6771bb580a8de428e
harlen lopez <harlen.d.lopez@gmail.com>
2020-01-20 22:04:18 -0800

    all but 3 and stretch done

--- code-challenges/filter/challenges-08.test.js
+++ code-challenges/filter/challenges-08.test.js
@@ -39,9 +39,11 @@ Write a function named notInFirstArray that, given two arrays as input, uses fil
 
 For example, notInFirstArray([1,2,3], [1,2,3,4]) returns [4].
 ------------------------------------------------------------------------------------------------ */
-
+//not quite sure on this one, its something easy im sure but im not seeing it.
 const notInFirstArray = (forbiddenValues, arr) => {
-  
+  return arr.filter(function(value) {
+    return this.index(value) < 0;
+  })
 };
 
 /* ------------------------------------------------------------------------------------------------
@@ -84,7 +86,7 @@ const snorlaxData = {
 };
 
 const getBaseStatGreaterThan = (arr, minBaseStat) => {
-  // Solution code here...
+  return arr.filter(stats => stats.baseStat > minBaseStat ? stats : null);
 };
 
 /* ------------------------------------------------------------------------------------------------
@@ -96,7 +98,14 @@ For example, getStatName(snorlaxData.stats, 50) will return ['special-defense', 
 ------------------------------------------------------------------------------------------------ */
 
 const getStatName = (arr, minBaseStat) => {
-  // Solution code here...
+  let result = [];
+  arr.filter(value => {
+    if (minBaseStat < value.baseStat){
+      result.push(value.stat.name)
+    }
+    return false;
+  });
+  return result;
 };
 
 /* ------------------------------------------------------------------------------------------------
@@ -149,7 +158,13 @@ const characters = [
 ];
 
 const getCharactersWithoutChildren = (arr) => {
-  // Solution code here...
+  let result = arr.filter(value => {
+    if (!Object.keys(value).includes('children')){
+      return true
+    }
+    return false;
+  });
+  return result;
 };
 
 /* ------------------------------------------------------------------------------------------------
